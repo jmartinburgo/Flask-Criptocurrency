8a536d4753d168c083fe886f5143a39edb9c817e
Juan Martin Burgo Mangiantini <jmartinburgo@hotmail.com>
2023-08-15 14:45:13 -0300

    Flask implementation

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
+from passlib.hash import sha256_crypt
+from flask_mysqldb import MySQL
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app=Flask(__name__)
+
+app.config['MYSQL_HOST']=os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER']=os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD']=os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB']=os.getenv('MYSQL_DB')
+app.config['MYSQL_CURSORCLASS']='DictCursor'
+
+mysql=MySQL(app)
+
+@app.route('/')
+def index():
+    return "Hello word"
+
+if __name__ == '__main__':
+    app.secret_key='secret123'
+    app.run(debug=True)
--- blockchain.py
+++ blockchain.py
@@ -29,27 +29,69 @@ class Block():
         ) 
     
     def __str__(self):
-        return str("Block#: %s\nHash: %s\nPrevious: %s\nData: %s\nNonce: %s\n" %(self.number, self.hash(),self.previous_hash, self.data, self.nonce))
+        return str("Block#: %s\nHash: %s\nPrevious: %s\nData: %s\nNonce: %s\n" %(
+            self.number, 
+            self.hash(),
+            self.previous_hash, 
+            self.data, 
+            self.nonce))
     
 
 
 class Blockchain():
-    difficulty= 4
+    difficulty=4
 
-    def __init__(self,chain[]):
+    def __init__(self,chain=[]):
         self.chain=chain
         
     def add(self,block):
-        self.chain.append({'hash':block.hash(),'previous':block.previous_has, 'number':block.number,'data':block.data, 'nonce':block.nonce})
+        self.chain.append(block)
+    
+    def remove(self,block):
+        self.chain.remove(block)
 
 
+    def mine(self,block):
+        try:
+            block.previous_hash=self.chain[-1].hash()
+        except IndexError:
+            pass
+        while True:
+            if block.hash()[:self.difficulty] == "0"* self.difficulty:
+                self.add(block);break
+            else:
+                block.nonce += 1
 
+    def isValid(self):
+        for i in range(1,len(self.chain)):
+            _previous = self.chain[i].previous_hash
+            _current=self.chain[i-1].hash()
+            if _previous != _current or _current[:self.difficulty] != "0" * self.difficulty:
+                return False
+        return True
 
 
 
 
 def main():
-    block= Block("Hello word",1)
+    blockchain =Blockchain()
+    database=["helloword","What's up","hello","bye"]
+
+    num=0
+    for data  in database:
+        num += 1 
+        blockchain.mine(Block(data,num))
+    
+    for block in blockchain.chain:
+        print(block)
+
+    blockchain.chain[2].data ="NEW DATA"
+    blockchain.mine(blockchain.chain[2])
+
+    
+    print(blockchain.isValid())
+
+
     
 
 if __name__=='__main__':
