e0eaf8ac9c30a913b771893e6bb3472fbd98dfad
Juan Martin Burgo Mangiantini <jmartinburgo@hotmail.com>
2023-08-16 16:52:16 -0300

    Mysql Access

--- app.py
+++ app.py
@@ -16,9 +16,11 @@ app.config['MYSQL_CURSORCLASS']='DictCursor'
 
 mysql=MySQL(app)
 
+
+
 @app.route('/')
 def index():
-    return "Hello word"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.secret_key='secret123'
